9195da5024d048cdfa22739a173fb0c48b2689b3
Ernest Tan <evilernie1985@gmail.com>
2017-10-01 03:22:27 +0800

    Added jQuery CDN. Added script.js. Canvas height and width set with JS.

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  console.log('Document ready')
+
+  var $canvas = $("canvas")
+  $canvas.width(window.innerWidth)
+  $canvas.height(window.innerHeight)
+
+  var c = canvas.getContext("2d")
+
+})
+
+// var canvas = document.querySelector('canvas')
+//
+// canvas.width = window.innerWidth

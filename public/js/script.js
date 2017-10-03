370905555ad26c66ca39922aa43d97ddff3c6256
Ernest Tan <evilernie1985@gmail.com>
2017-10-03 18:50:44 +0800

    Added x and y-axis interactivity for star field. Added infinite scrolling stars for x and y -axis.

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,45 @@
 $(document).ready(function () {
   console.log('Document ready')
 
+// Canvas Init ========================
+
   var canvas = $('#canvas')
   canvas[0].width = window.innerWidth
   canvas[0].height = window.innerHeight
   var c = canvas[0].getContext('2d')
 
+// Responsive Canvas Resize ===============
+
+  // $(window).resize(function () {
+  //   canvas[0].width = window.innerWidth
+  //   canvas[0].height = window.innerHeight
+  //   c.clearRect(0, 0, window.innerWidth, window.innerHeight)
+  // })
+
+// Canvas Intereactions ====================
+
+  var mouse = {
+    x: undefined,
+    y: undefined
+  }
+
+  $(window).mousemove(function (event) {
+    // console.log('X: ' + event.originalEvent.movementX)
+    // console.log('Y: ' + event.originalEvent.movementY)
+    mouse.x = event.originalEvent.movementX
+    mouse.y = event.originalEvent.movementY
+    // is this not an integer?
+  })
+
 // Star Object ======================
 
-  function Star (x, y, width, height, dx) {
+  function Star (x, y, width, height, dx, dy) {
     this.x = x
     this.y = y
     this.width = width
     this.height = height
     this.dx = dx
+    this.dy = dy
 
     // draws star ===================
 
@@ -27,9 +53,30 @@ $(document).ready(function () {
     this.update = function () {
       // infinitely scrolling stars!
       if (this.x > window.innerWidth) {
-        this.x = 0
+        this.x = 0 - Math.random() * window.innerWidth
+      } else if (this.x < 0) {
+        this.x = window.innerWidth + Math.random() * window.innerWidth
       }
-      this.x += this.dx
+
+      if (this.y > window.innerHeight) {
+        this.y = 0 - Math.random() * window.innerHeight
+      } else if (this.y < 0) {
+        this.y = window.innerHeight + Math.random() * window.innerHeight
+      }
+
+      // Star interactivity
+      if (mouse.x == undefined) {
+        this.x += this.dx
+      } else {
+        this.x += this.dx + (mouse.x * 0.15)
+      }
+
+      if (mouse.y == undefined) {
+        this.y += dy
+      } else {
+        this.y += this.dy + (mouse.y * 0.15)
+      }
+
       this.draw()
     }
   }
@@ -42,7 +89,8 @@ $(document).ready(function () {
     let x = Math.random() * window.innerWidth
     let y = Math.random() * window.innerHeight
     let dx = 0.2
-    backStarArray.push(new Star(x, y, 1, 1, dx))
+    let dy = 0
+    backStarArray.push(new Star(x, y, 1, 1, dx, dy))
   }
 
   // Foreground Stars =======================
@@ -53,7 +101,8 @@ $(document).ready(function () {
     let x = Math.random() * window.innerWidth
     let y = Math.random() * window.innerHeight
     let dx = 0.4
-    foreStarArray.push(new Star(x, y, 2, 2, dx))
+    let dy = 0
+    foreStarArray.push(new Star(x, y, 2, 2, dx, dy))
   }
 
 // Animate =======================
@@ -71,5 +120,4 @@ $(document).ready(function () {
   }
 
   animate()
-
 })
